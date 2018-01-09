252d5b5e21cc2ccbdade4d88fe50b6883c354002
israe <israe@Lenovo-PC>
2018-01-08 21:21:43 -0600

    Se agrega Interfaz Esaud

---
+++ src/mx/esaud/productos/interfaces/TicketEsaud.java
@@ -0,0 +1,13 @@
+package mx.esaud.productos.interfaces;
+
+/**
+ *
+ * @author israe
+ */
+public interface TicketEsaud {
+    
+    public void hola();
+    public void como();
+    public void estas();
+    
+}
