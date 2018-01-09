5233892b79539ed05b351825e06cf903112b2947
Windows 10 Pro <Windows 10 Pro@acer>
2018-01-08 22:39:03 -0600

    Se agrega la interface TicketJosafat

---
+++ src/mx/esaud/productos/interfaces/TicketJosafat.java
@@ -0,0 +1,14 @@
+package mx.esaud.productos.interfaces;
+
+/**
+ *
+ * @author Windows 10 Pro
+ */
+public interface TicketJosafat {
+    
+    public void hola();
+    
+    public void soy();
+    
+    public void josa();
+}
